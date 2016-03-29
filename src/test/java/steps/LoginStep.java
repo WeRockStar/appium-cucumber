75a3aa115e0df62b01ca3f07ea38829eb4bf163b
WeRockStar <vr_freestyle@outlook.com>
2016-03-29 17:51:07 +0700

    Implement steps

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -7,6 +7,10 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class LoginStep {
 
@@ -19,27 +23,31 @@ public class LoginStep {
 
     @Given("^I am on \"([^\"]*)\" page$")
     public void i_am_on_page(String pageName) throws Throwable {
-
+        driver.startActivity("com.werockstar.cumcumber", "com.werockstar.cumcumber.LoginActivity");
     }
 
     @When("^I fill in username with \"([^\"]*)\"$")
     public void i_fill_in_username_with(String username) throws Throwable {
-
+        WebElement editTextUsername = driver.findElement(By.id("com.werockstar.cumcumber:id/editTextUserName"));
+        editTextUsername.sendKeys(username);
     }
 
     @When("^I fill in password with \"([^\"]*)\"$")
     public void i_fill_in_password_with(String password) throws Throwable {
-
+        WebElement editTextPassword = driver.findElement(By.id("com.werockstar.cumcumber:id/editTextPassword"));
+        editTextPassword.sendKeys(password);
     }
 
     @When("^I press \"([^\"]*)\" button$")
     public void i_press_button(String button) throws Throwable {
-
+        WebElement btnLogin = driver.findElement(By.name(button));
+        btnLogin.click();
     }
 
-    @Then("^I should be on Member page$")
-    public void i_should_be_on_Member_page() throws Throwable {
-
+    @Then("^I should see message \"([^\"]*)\"$")
+    public void i_should_see_message(String message) throws Throwable {
+        WebElement dialogMessage = driver.findElement(By.id("android:id/message"));
+        assertEquals(message, dialogMessage.getText());
     }
 
     @After
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,7 +1,7 @@
 Feature: Login
   Scenario: Member login with e-mail successful
     Given I am on "Login" page
-    When  I fill in username with "user"
-    And   I fill in password with "password"
+    When  I fill in username with "root"
+    And   I fill in password with "root"
     And   I press "LOGIN" button
-    Then  I should be on Member page
+    Then  I should see message "Welcome"
